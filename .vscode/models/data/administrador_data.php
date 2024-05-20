d4a90601a12205a00e4f9cd7e73284f58570f104
carlosBurgos12 <129403096+carlosBurgos12@users.noreply.github.com>
2024-05-20 12:05:14 -0600

    cambios

--- .vscode/models/data/administrador_data.php
+++ .vscode/models/data/administrador_data.php
@@ -2,7 +2,7 @@
 // Se incluye la clase para validar los datos de entrada.
 require_once('../../../helper/validator.php');
 // Se incluye la clase padre.
-require_once('../../models/handler/administrador_handler.php');
+require_once('../models/handler/administrador_handler.php');
 /*
  *  Clase para manejar el encapsulamiento de los datos de la tabla USUARIO.
  */
