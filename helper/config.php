d88ea086b15d5f0c96b63fafb0c6e2051e7fcf2a
DenisCastillo06 <136492540+DenisCastillo06@users.noreply.github.com>
2024-05-19 18:22:23 -0600

    Update config.php

--- helper/config.php
+++ helper/config.php
@@ -1,11 +1,13 @@
 <?php
 // Encabezado para permitir solicitudes de cualquier origen.
 header('Access-Control-Allow-Origin: *');
+
 // Se establece la zona horaria local para la fecha y hora del servidor.
 date_default_timezone_set('America/El_Salvador');
+
 // Constantes para establecer las credenciales de conexión con el servidor de bases de datos.
-define('SERVER', 'localhost');
-define('DATABASE', 'streetweardrop_db');
-define('USERNAME', 'root');
-define('PASSWORD', '');
+define('SERVER', 'localhost'); // Dirección del servidor de base de datos
+define('DATABASE', 'streetweardrop_db'); // Nombre de la base de datos
+define('USERNAME', 'root'); // Nombre de usuario de la base de datos
+define('PASSWORD', ''); // Contraseña de la base de datos
 ?>
